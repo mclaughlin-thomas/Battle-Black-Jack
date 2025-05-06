6f7bfbce59680baa6555f54efb30f4488f1141bb
Thomas McLaughlin <90363044+mclaughlin-thomas@users.noreply.github.com>
2025-05-05 22:28:16 -0400

    Update blackjackHandler.ts

--- webapp/src/blackjackHandler.ts
+++ webapp/src/blackjackHandler.ts
@@ -33,6 +33,7 @@ function createDeck(): Card[] {
     return deck;
 }
 
+// https://medium.com/@khaledhassan45/how-to-shuffle-an-array-in-javascript-6ca30d53f772
 function shuffleDeck(deck: Card[]): Card[] {
     for (let i = deck.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
